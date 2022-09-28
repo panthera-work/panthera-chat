e1c672543a04d9129fff9aeb61c0e9b690559b9d
panthera <panthera-work@yandex.ru>
2022-09-28 22:04:28 +0700

    Инициализация проекта

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+    return (
+        <div className="App">
+            <div>
+                Чаты
+            </div>
+            <div>
+                определенный чат
+            </div>
+            <div>
+                сообщения чата
+            </div>
+            
+        </div>
+    );
+}
+
+export default App;
